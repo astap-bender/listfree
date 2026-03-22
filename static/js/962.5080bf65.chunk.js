14db2a855d0338b0b7dd0535ea56515c1fb2ab03
bender <astapbender394@mail.ru>
2026-03-22 20:25:45 +0300

    Updates

---
+++ static/js/962.5080bf65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkset=globalThis.webpackChunkset||[]).push([[962],{962(t,e,r){r.d(e,{oI6:()=>i});var o=r(441);function i(t){return(0,o.k5)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"1.5",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M15.75 5.25v13.5m-7.5-13.5v13.5"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=962.5080bf65.chunk.js.map
